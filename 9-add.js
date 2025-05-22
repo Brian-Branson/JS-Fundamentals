1ccdcefbeb3db2f6b2066bfbf5bd8b6c41015788
Brian <karanjabrian382@gmail.com>
2025-05-23 00:45:16 +0300

    9-add.js completed

--- 9-add.js
+++ 9-add.js
@@ -3,7 +3,7 @@ const first = Number(args[2]);
 const second = Number(args[3]);
 
 if (isNaN(first) || isNaN(second)) {
-    console.log('NAN');
+    console.log('NaN');
 } else {
     console.log(first + second);
 }
