02d9d97c98fa3cd7ac9f4ceb7765c3ee54748251
Brian <karanjabrian382@gmail.com>
2025-05-22 23:53:59 +0300

    6-multi_languages_loop.js completed

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,4 @@
-const line = [ " C is fun", "Python is cool", "JavaScript is amazing"];
-
-while (i < line.length) {
+const line = [ "C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < line.length; i++) {
     console.log(line[i]);
-    i++;
 }
