dcf2a06aefb8b716eb3d65de86f61efe94e86f60
Brian <karanjabrian382@gmail.com>
2025-05-22 23:37:39 +0300

    complete 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,5 +2,5 @@ const argv = process.argv;
 const flooredNumber = Math.floor(originalNumber);
 
 if (!Number.isNaN(Number(argv[2])))
-console.log(' My number is : ' ,flooredNumber);
+console.log(' My number : ' ,flooredNumber);
 else console.log('Not a number');
